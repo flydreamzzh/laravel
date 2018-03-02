ac297a1e038526990417fbec669e11a6b05e0f98
张志辉 <mr.zhang@zhangzhihuideMacBook-Pro.local>
2018-03-02 12:08:24 +0800

    权限管理

--- resources/views/auth_permissions/menu_permissions.blade.php
+++ resources/views/auth_permissions/menu_permissions.blade.php
@@ -1,20 +1,25 @@
 <div class="layui-card-header p6">
     <div class="pull-left" style="height: auto;width: 200px">
-        {{--<input type="text" class="layui-input" style="height: 30px">--}}
     <input type="text" name="title" lay-verify="title" autocomplete="off" style="height: 30px" placeholder="请输入标题" class="layui-input">
     </div>
     <button class="pull-right btn btn-sm btn-dark" onclick="addPermission()"><i class="fa fa-plus"></i> 新增</button>
 </div>
-<table class="layui-hide" id="permissions" lay-filter="usersTable"></table>
-<div class="pl5" style="width: 100%;height: auto;margin-bottom: 0;">
+<table class="layui-hide" id="permissions" lay-filter="permissions"></table>
+<div class="pl5" id="permission_btns" style="position:absolute; bottom: 0;width: 100%;height: 40px;">
     <input type="button" disabled class="btn btn-sm btn-success" value="启用">
     <input type="button" disabled class="btn btn-sm btn-default" value="禁用">
     <input type="button" disabled class="btn btn-sm btn-danger" value="删除">
 </div>
 
+<script type="text/html" id="permission">
+    <!-- 这里的 checked 的状态只是演示 -->
+    <a href="javascript:void(0)" class="text-info" onclick="updatePermission('@{{ d.id }}')">@{{ d.name }}</a>
+</script>
+
+
 <script type="text/html" id="switchTpl">
     <!-- 这里的 checked 的状态只是演示 -->
-    <input type="checkbox" name="sex" value="@{{ d.status }}" lay-skin="switch" lay-text="启用|禁用" lay-filter="sexDemo" @{{ d.status == 0 ? 'checked' : false }} >
+    <input disabled type="checkbox" name="sex" value="@{{ d.status }}" lay-skin="switch" lay-text="启用|禁用" @{{ d.status == 0 ? 'checked' : false }} >
 </script>
 
 <script type="text/javascript">
@@ -25,21 +30,31 @@
             url: '{{ route("authPermissions.permissions") }}',
             cols: [[
                 {type: 'checkbox'},
-                {field:'name', title:'名称'},
-                {field:'route', title:'路由'},
+                {field:'name', title:'名称', width:120, templet: '#permission'},
+                {field:'route', title:'路由', width:120},
                 {field:'description', title:'描述'},
-                {field:'status', title:'状态', width:85, templet: '#switchTpl', unresize: true}
+                {field:'status', title:'状态', width:95, templet: '#switchTpl', unresize: true}
             ]],
-            id: 'users',
+            id: 'permissions',
             limit: 15,
             limits:[15,30,50,100],
             page: true,
-            height: '400px'
+            height: '360px'
         });
-//        //监听性别操作
-//        form.on('switch(sexDemo)', function(obj){
-//            layer.tips(this.value + ' ' + this.name + '：'+ obj.elem.checked, obj.othis);
-//        });
+
+        table.on('checkbox(permissions)', function(obj){
+            var checkStatus = table.checkStatus('permissions')
+                , data = checkStatus.data;
+            if (data.length) {
+                $("#permission_btns input[type=button]").prop('disabled', false);
+            } else {
+                $("#permission_btns input[type=button]").prop('disabled', true);
+            }
+        });
     });
+
+    function updatePermission(permission_id) {
+        alert(permission_id)
+    }
 
 </script>
--- resources/views/auth_permissions/table.blade.php
+++ resources/views/auth_permissions/table.blade.php
@@ -1,32 +1,33 @@
 @php($menus = (new \App\Models\Menu())->tree_lastNodes())
-
-<div class="layui-collapse" lay-filter="test">
+<div class="layui-collapse">
     <form class="layui-form layui-form-pane" action="">
     @foreach($menus as $menu)
         <div class="layui-colla-item">
             <h2 class="layui-colla-title">
                 {{ $menu->name }}
-                <div class="pull-right">
-                    <input type="button" class="layui-btn layui-btn-primary layui-btn-xs" value="编辑" onclick="editPermission('{{ $menu->id }}', '{{ $menu->name }}')">
-                </div>
             </h2>
-            <div class="layui-form-item">
-                <input type="checkbox" name="like1[write]" lay-skin="primary" title="写作" checked="">
-                <input type="checkbox" name="like1[read]" lay-skin="primary" title="阅读">
-                <input type="checkbox" name="like1[game]" lay-skin="primary" title="游戏" disabled="">
-                <input type="checkbox" name="like1[1]" lay-skin="primary" title="写作" checked="">
-                <input type="checkbox" name="like1[2]" lay-skin="primary" title="阅读">
-                <input type="checkbox" name="like1[3]" lay-skin="primary" title="游戏" disabled="">
+            <div class="pull-right pr10" style="position: relative;margin-top: -32px">
+                <input type="button" class="layui-btn layui-btn-primary layui-btn-xs" value="编辑" onclick="editPermission('{{ $menu->id }}', '{{ $menu->name }}')">
+            </div>
+            <div class="layui-colla-content layui-show">
+                <div class="layui-form-item">
+                    <input type="checkbox" name="like1[write]" lay-skin="primary" title="写作" checked="">
+                    <input type="checkbox" name="like1[read]" lay-skin="primary" title="阅读">
+                    <input type="checkbox" name="like1[game]" lay-skin="primary" title="游戏" disabled="">
+                    <input type="checkbox" name="like1[1]" lay-skin="primary" title="写作" checked="">
+                    <input type="checkbox" name="like1[2]" lay-skin="primary" title="阅读">
+                    <input type="checkbox" name="like1[3]" lay-skin="primary" title="游戏" disabled="">
+                </div>
             </div>
         </div>
     @endforeach
     </form>
 </div>
 <script type="text/javascript">
-    layui.use(['form'], function() {
-        var form = layui.form;
+    layui.use(['form', 'element'], function() {
+        var form = layui.form, element = layui.element;
         form.render();
-    })
+    });
 
     function editPermission(menu_id, title) {
         $.get("{{ route("authPermissions.menuPermissions") }}", {}, function (response) {
